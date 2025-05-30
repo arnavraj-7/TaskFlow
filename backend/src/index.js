b980b633a01cb65d10a9de5d8e555c904a08719a
Arnav Raj <arnavrajcodes@gmail.com>
2025-05-30 18:15:20 +0530

    cors fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,8 @@ const app=express();
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
 app.use('/api/todos', todosRoutes);
